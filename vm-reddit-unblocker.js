05cd81e2f2d96f52fdf92264dbefa2cd3fc15513
Khai <contact@8mpty.com>
2025-09-24 22:43:50 +0800

    Create vm-reddit-unblocker.js

---
+++ vm-reddit-unblocker.js
@@ -0,0 +1,74 @@
+// ==UserScript==
+// @name        Reddit NSFW Unblocker
+// @namespace   8mpty Script
+// @match        *://*.reddit.com/r/*
+// @match        *://*.reddit.com/user/*
+// @match        *://*.reddittorjg6rue252oqsxryoxengawnmo46qy4kyii5wtqnwfj4ooad.onion/r/*
+// @match        *://*.reddittorjg6rue252oqsxryoxengawnmo46qy4kyii5wtqnwfj4ooad.onion/user/*
+// @grant       none
+// @version     1.0
+// @author      8mpty
+// @description Unblurs Reddits NSFW contents on web browsers
+// ==/UserScript==
+
+unblock();
+
+/**
+ * On further page navigation, in some cases needed, in others not:
+ *  - when navigating from a /r/subreddit to a post, the content is loaded dynamically (and vice versa) -> extension "content_scripts" is not executed
+ *  - when jumping from a post to a recommended post, then a page load is executed
+ */
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation.target.nodeName.toLowerCase() === "shreddit-app") {
+            unblock();
+        }
+    });
+});
+
+observer.observe(document.querySelector("shreddit-app"), {
+    childList: true,
+});
+
+function unblock() {
+    document.body.style = "";
+
+    /**
+     * This blur element is dynamically added when navigating into a post or from a post to the subreddit.
+     * This element is not added when navigating to recommended posts.
+     * Just in case, always locate it and remove it.
+     */
+    document.querySelector("div[style*='blur(4px)']")?.remove();
+
+    document.getElementById("nsfw-qr-dialog")?.remove();
+    document.getElementById("blocking-modal")?.remove();
+
+    /**
+     * New elements to also remove.
+     * As the IDs of the elements suggest, this seems to be desktop only.
+     * On mobile/smaller screens these elements are not present.
+     */
+    document.getElementById("nsfw-desktop-auth-blocking-modal-overlay-element")?.remove();
+    document.getElementById("nsfw-desktop-auth-blocking-modal-dialog")?.remove();
+    document.getElementById("nsfw-desktop-auth-blocking-modal")?.remove();
+
+    /**
+     * On navigating into a post or from a post into a cross-post,
+     * wait a minimum amount before trying to delete the post overlay prompt,
+     * because at first the element we are looking for does not seem to be loaded/present yet
+     */
+    setTimeout(() => {
+        var blockingContainers = document.getElementsByTagName("xpromo-nsfw-blocking-container");
+        for (var container of blockingContainers) {
+            container?.shadowRoot.children[1].remove();
+        }
+    }, 1);
+
+    /**
+     * Remove the "untagged blocking" modal, that warns that the content is not reviewed
+     * and it prompts the user to use the Reddit app or go to the home page.
+     * This popup seems to only appear on smaller screen sizes, e.g. on mobile.
+     * This can be replicated by using the browsers devtools and emulating a mobile device.
+     */
+    document.getElementsByTagName("xpromo-untagged-content-blocking-modal")[0]?.remove();
+}
